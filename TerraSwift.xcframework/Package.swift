9965431e1416e22bb03e1b9fb9e3e16af8833296
Elliott Yu <elliottyu1@gmail.com>
2021-10-05 17:01:22 +0100

    Package File

--- TerraSwift.xcframework/Package.swift
+++ TerraSwift.xcframework/Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Terra-Swift",
-            targets: ["Terra-Swift"]),
+            name: "TerraSwift",
+            targets: ["TerraSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,11 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Terra-Swift",
-            dependencies: []),
-        .testTarget(
-            name: "Terra-SwiftTests",
-            dependencies: ["Terra-Swift"]),
+        .binaryTarget(
+            name: "TerraSwift",
+            path: "TerraSwift.xcframework"),
     ]
 )
