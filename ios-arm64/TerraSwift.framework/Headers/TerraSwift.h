44972a9bb2e83d0e2d43c6e2f67000961df9a907
Elliott Yu <elliottyu1@gmail.com>
2021-10-05 16:44:29 +0100

    Framework files

---
+++ ios-arm64/TerraSwift.framework/Headers/TerraSwift.h
@@ -0,0 +1,18 @@
+//
+//  TerraSwift.h
+//  TerraSwift
+//
+//  Created by Elliott Yu on 05/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TerraSwift.
+FOUNDATION_EXPORT double TerraSwiftVersionNumber;
+
+//! Project version string for TerraSwift.
+FOUNDATION_EXPORT const unsigned char TerraSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TerraSwift/PublicHeader.h>
+
+
