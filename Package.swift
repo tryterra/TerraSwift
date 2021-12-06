c059a00a0e62e85c110cc695933488a070f8e586
Elliott Yu <elliottyu1@gmail.com>
2021-12-06 11:07:01 +0000

    Revert Package Dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TerraSwift",
     platforms: [
-        .iOS(.v13), .iOS(.v14), .iOS(.v15)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
